b54b627e3ffb557abe65f205149d77884d51e9a1
DHoper <gxgemini777@gmail.com>
2023-10-29 15:14:29 +0800

    fix-修正env設定及引入

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ const userRoutes = require("../api/userRoutes");
 const commentRoutes = require("../api/commentRoutes");
 const communityRoutes = require("../api/communityRoutes");
 const communityCommentRoutes = require("../api/communityCommentRoutes");
+require('dotenv').config();
 
 const app = express();
-const dbUrl =
-  "mongodb+srv://gxgemini777:apollo777@wildlens.0vgoc5r.mongodb.net/main?retryWrites=true&w=majority";
 
+const port = process.env.PORT || 3000;
+const dbUrl = process.env.DBUrl;
 (async () => {
   await mongoose.connect(dbUrl);
   return "DB連接成功!!!";
@@ -27,7 +28,6 @@ app.use("/", commentRoutes);
 app.use("/", communityRoutes);
 app.use("/", communityCommentRoutes);
 
-
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("程序運行於 3000 端口中");
 });
